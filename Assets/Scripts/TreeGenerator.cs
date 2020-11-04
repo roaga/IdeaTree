309c435c5d69a09b872b7ef13a78dc7fb8708558
Rohan Agarwal <ro.agarwal@hotmail.com>
2020-11-04 15:10:50 -0500

    Fix spawn location

--- Assets/Scripts/TreeGenerator.cs
+++ Assets/Scripts/TreeGenerator.cs
@@ -20,6 +20,8 @@ public class TreeGenerator : MonoBehaviour {
 
     public void SpawnTree(Vector3 basePos) {
         basePosition = basePos;
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
 
         Branch rootBranch = new Branch("", null, null, 1);
         branches = new List<Branch>();
@@ -30,15 +32,12 @@ public class TreeGenerator : MonoBehaviour {
         vertices = new List<Vector3>();
         triangles = new List<int>();
         for (double angle = 0.0; angle < 360.0; angle +=  360.0 / NUM_VERTICES_IN_SHAPE) {
-            vertices.Add(new Vector3(basePosition.x + thicknessFactor * Math.Cos(angle), basePosition.y, basePosition.z + thicknessFactor * Math.Sin(angle)));
+            vertices.Add(new Vector3(thicknessFactor * (float) Math.Cos(angle), 0, thicknessFactor * (float) Math.Sin(angle)));
         }
 
-        float[] verticesToAdd = CalculateBranch(new Vector3(0, 0, 0), basePos, heightFactor, rootBranch.GetLevelNum());
-        for (int i = 0; i < verticesToAdd.Length; i++) {
-            vertices.Add(verticesToAdd[i]);
-        }
+        CalculateBranch(new Vector3(0, 0, 0), basePos, heightFactor, rootBranch.GetLevelNum());
 
-        CreateShape();
+        UpdateMesh();
     }
 
     void NewBranch() {
@@ -47,38 +46,33 @@ public class TreeGenerator : MonoBehaviour {
 
     }
 
-    float[] CalculateBranch(Vector3 rotation, Vector3 rootPos, float length, int levelNum) {
-        float[] vertices = new float[NUM_VERTICES_IN_SHAPE * 2];
-
+    void CalculateBranch(Vector3 rotation, Vector3 rootPos, float length, int levelNum) {
         // calculate center of top face using rotation and length factor
-        Vector3 topCenter = new Vector3(basePosition.x, basePosition.y + heightFactor / (levelNum * 0.5), basePosition.z);
+        Vector3 topCenter = new Vector3(0, heightFactor / (levelNum * 0.5f), 0);
         Vector3 dir = topCenter - rootPos;
-        dir = Quaternion.Euler(rotatation) * dir;
+        dir = Quaternion.Euler(rotation) * dir;
         topCenter = dir + rootPos;
 
         // calculate top vertices
         float thickness = thicknessFactor * numLevels * 0.5f / levelNum;
         for (double angle = 0.0; angle < 360.0; angle +=  360.0 / NUM_VERTICES_IN_SHAPE) {
-            vertices.Add(new Vector3(topCenter.x + thickness * Math.Cos(angle), topCenter.y, topCenter.z + thickness * Math.Sin(angle)));
+            vertices.Add(new Vector3(topCenter.x + thickness * (float) Math.Cos(angle), topCenter.y, topCenter.z + thickness * (float) Math.Sin(angle)));
         }
 
         // Calculate triangles using previous vertices as base; there should be 2 * NUM_VERTICES_IN_SHAPE = 12 triangles
         for (int vertex = (branches.Count - 1) * 2; vertex < NUM_VERTICES_IN_SHAPE; vertex++) {
-            triangles.Add(vertices[vertex]);
+            triangles.Add(vertex);
             if ((vertex + 1) % NUM_VERTICES_IN_SHAPE == 0) {
-                triangles.Add(vertices[vertex + 1 - NUM_VERTICES_IN_SHAPE]);
+                triangles.Add(vertex + 1 - NUM_VERTICES_IN_SHAPE);
             } else {
-                triangles.Add(vertices[vertex + 1]);
+                triangles.Add(vertex + 1);
             }
-            triangles.Add(vertices[vertex + NUM_VERTICES_IN_SHAPE - 1]);
+            triangles.Add(vertex + NUM_VERTICES_IN_SHAPE - 1);
 
-            triangles.Add(vertices[vertex]);
-            triangles.Add(vertices[vertex + NUM_VERTICES_IN_SHAPE - 1]);
-            triangles.Add(vertices[vertex + NUM_VERTICES_IN_SHAPE]);
+            triangles.Add(vertex);
+            triangles.Add(vertex + NUM_VERTICES_IN_SHAPE - 1);
+            triangles.Add(vertex + NUM_VERTICES_IN_SHAPE);
         }
-
-
-        return vertices;
     }
 
     void CalculateLengths() { 
@@ -87,13 +81,6 @@ public class TreeGenerator : MonoBehaviour {
 
     void CalculateThicknesses() {
         
-    }
-
-    void CreateShape() {
-        Debug.Log("TODO: generate tree mesh");
-
-
-        UpdateMesh();
     }
     
     void UpdateMesh() {
