6fc583ee172eccfc07d5bf4c82e56343fd6f6cec
Rohan Agarwal <ro.agarwal@hotmail.com>
2020-11-12 19:04:11 -0500

    Revert

--- Assets/Scripts/TreeGenerator.cs
+++ Assets/Scripts/TreeGenerator.cs
@@ -61,8 +61,8 @@ public class TreeGenerator : MonoBehaviour {
         float horizontalOffset = -80 + (siblings.Count - 1) * 10f; 
         
         // apply vertical rotation
-        rotation.x += verticalOffset * 0.5;
-        rotation.z += verticalOffset * 0.5;
+        rotation.x += verticalOffset * Math.Sin(rotation.x);
+        rotation.z += verticalOffset * Math.Cos(rotation.z);
 
         // apply horizontal rotation
         rotation.y += horizontalOffset;
